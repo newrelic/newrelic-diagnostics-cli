1785b60001b019a2ca2db2bb21d8755723b0ffb1
Zach Leslie <zleslie@newrelic.com>
2020-11-05 10:53:44 -0800

    feat(hostinfo): include process information

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/newrelic/newrelic-diagnostics-cli
+module github.com/zlesnr/newrelic-diagnostics-cli
 
 go 1.14
 
@@ -9,14 +9,17 @@ require (
 	github.com/go-ole/go-ole v1.2.4 // indirect
 	github.com/google/uuid v1.1.2
 	github.com/gorilla/mux v1.8.0
+	github.com/kr/pretty v0.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/onsi/ginkgo v1.14.2
 	github.com/onsi/gomega v1.10.3
 	github.com/shirou/gopsutil v2.20.9+incompatible
 	github.com/shirou/w32 v0.0.0-20160930032740-bb4de0191aa4
+	github.com/sirupsen/logrus v1.7.0
 	github.com/stretchr/testify v1.6.1 // indirect
 	golang.org/x/sys v0.0.0-20201013132646-2da7054afaeb
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/cheggaaa/pb.v1 v1.0.28
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776
 )
--- tasks/base/env/hostInfoHelpers.go
+++ tasks/base/env/hostInfoHelpers.go
@@ -1,81 +1,121 @@
 package env
 
 import (
-	"runtime"
 	"context"
-	"github.com/shirou/gopsutil/host"
-	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/mem"
 	"errors"
+	"fmt"
+	"runtime"
 	"strings"
+
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/process"
 )
 
 //Defines the struct for holding host name and OS information
 type HostInfo struct {
-	Hostname        string
-	OS              string
-	Platform        string
-	PlatformFamily  string
-	PlatformVersion string
-	KernelVersion 	string
-	KernelArch		string
-	CPUs			[]CPU
+	Hostname             string
+	OS                   string
+	Platform             string
+	PlatformFamily       string
+	PlatformVersion      string
+	KernelVersion        string
+	KernelArch           string
+	CPUs                 []CPU
 	TotalVirtualMemoryMB int
+	Processes            []Process
 }
 
 type CPU struct {
 	Cores int32
-	Mhz float64
+	Mhz   float64
 }
 
-type HostInfoProviderFunc func()(HostInfo, error)
-type HostInfoProviderWithContextFunc func(context.Context)(HostInfo, error)
+type Process struct {
+	name string
+	id   int32
+}
 
-func NewHostInfo()(HostInfo, error){
+// Name implements the genericProcess interface as part of the newrelic-cli discovery.
+func (p Process) Name() (string, error) {
+	return p.name, nil
+}
+
+type HostInfoProviderFunc func() (HostInfo, error)
+type HostInfoProviderWithContextFunc func(context.Context) (HostInfo, error)
+
+func NewHostInfo() (HostInfo, error) {
 	ctx := context.Background()
 	return NewHostInfoWithContext(ctx)
 }
 
-func NewHostInfoWithContext(ctx context.Context)(HostInfo, error) {
+func NewHostInfoWithContext(ctx context.Context) (HostInfo, error) {
 	hostInfo := HostInfo{}
 	errorMessages := []string{}
-	
-	hostInfoErr := populateHostInfo(&hostInfo, ctx)
 
+	hostInfoErr := populateHostInfo(&hostInfo, ctx)
 	if hostInfoErr != nil {
 		errorMessages = append(errorMessages, hostInfoErr.Error())
 	}
 
 	cpuInfoErr := populateHostCPUInfo(&hostInfo, ctx)
-
 	if cpuInfoErr != nil {
 		errorMessages = append(errorMessages, cpuInfoErr.Error())
 	}
 
 	memInfoErr := populateHostMemoryInfo(&hostInfo, ctx)
-
 	if memInfoErr != nil {
 		errorMessages = append(errorMessages, memInfoErr.Error())
 	}
 
-	if len(errorMessages ) > 0 {
+	processInfoErr := populateHostProcessInfo(&hostInfo, ctx)
+	if memInfoErr != nil {
+		errorMessages = append(errorMessages, processInfoErr.Error())
+	}
+
+	if len(errorMessages) > 0 {
 		return hostInfo, errors.New(strings.Join(errorMessages, "\n"))
 	}
 
 	return hostInfo, nil
 }
 
+func populateHostProcessInfo(hostInfo *HostInfo, ctx context.Context) error {
+	pids, err := process.PidsWithContext(ctx)
+	if err != nil {
+		return err
+	}
 
+	for _, pid := range pids {
+		p, err := process.NewProcess(pid)
+		if err != nil {
+			return fmt.Errorf("cannot read pid %d: %s", pid, err.Error())
+		}
 
+		name, err := p.Name()
+		if err != nil {
+			return fmt.Errorf("cannot read name of pid %d: %s", pid, err.Error())
+		}
 
+		proc := Process{
+			name: name,
+			id:   p.Pid,
+		}
+
+		hostInfo.Processes = append(hostInfo.Processes, proc)
+	}
+
+	return nil
+}
 
 func populateHostInfo(hostInfo *HostInfo, ctx context.Context) error {
 
 	info, err := host.InfoWithContext(ctx)
 	if err != nil {
 		hostInfo.OS = runtime.GOOS
-		return err;
-	} 
+		return err
+	}
 
 	hostInfo.OS = info.OS
 	hostInfo.Hostname = info.Hostname
@@ -91,13 +131,13 @@ func populateHostInfo(hostInfo *HostInfo, ctx context.Context) error {
 func populateHostCPUInfo(hostInfo *HostInfo, ctx context.Context) error {
 	info, err := cpu.InfoWithContext(ctx)
 	if err != nil {
-		return err;
-	} 
+		return err
+	}
 
 	for _, cpuInfo := range info {
 		cpu := CPU{
 			Cores: cpuInfo.Cores,
-			Mhz: cpuInfo.Mhz,
+			Mhz:   cpuInfo.Mhz,
 		}
 
 		hostInfo.CPUs = append(hostInfo.CPUs, cpu)
@@ -109,10 +149,10 @@ func populateHostCPUInfo(hostInfo *HostInfo, ctx context.Context) error {
 func populateHostMemoryInfo(hostInfo *HostInfo, ctx context.Context) error {
 	info, err := mem.VirtualMemoryWithContext(ctx)
 	if err != nil {
-		return err;
-	} 
+		return err
+	}
 
-	hostInfo.TotalVirtualMemoryMB =  int((info.Total) / uint64(1048576))
+	hostInfo.TotalVirtualMemoryMB = int((info.Total) / uint64(1048576))
 
 	return nil
 }
