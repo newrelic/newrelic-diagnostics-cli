728491ee63d29bbe75e6ca7b88c401df42bbe553
Hekate Nichols <hnichols@c02c3jvlmd6m.fios-router.home>
2021-01-07 15:34:55 -0800

    updating the way we check node version module

--- tasks/node/agent/version.go
+++ tasks/node/agent/version.go
@@ -37,12 +37,23 @@ func (t NodeAgentVersion) Dependencies() []string {
 // Execute - The core work within this task
 func (t NodeAgentVersion) Execute(options tasks.Options, upstream map[string]tasks.Result) tasks.Result {
 	var result tasks.Result
+	var nodeModuleVersions []NodeModuleVersion
 
 	if upstream["Node/Config/Agent"].Status != tasks.Success {
 		result.Status = tasks.None
 		result.Summary = "Node agent not detected"
 		return result
 	}
+	if upstream["Node/Env/Dependencies"].Status == tasks.Success {
+		nodeModuleVersions, ok = upstream["Node/Env/Dependencies"].Payload.([]NodeModuleVersion) 
+		if !ok {
+			return tasks.result {
+				Status: tasks.None,
+				Summary:  "Node module version not detected",
+			}
+		}
+	}
+
 	logs, ok := upstream["Base/Log/Copy"].Payload.([]logtask.LogElement)
 	if !ok {
 		result.Status = tasks.None
@@ -62,29 +73,42 @@ func (t NodeAgentVersion) Execute(options tasks.Options, upstream map[string]tas
 	return result
 }
 
-func getNodeVerFromLog(logs []logtask.LogElement) (agentVersion []logNodeAgentVersion) {
-	for _, logElement := range logs {
-		logAgentVersion, errSearchLogs := searchLogs(logElement)
-		var logElementNode logNodeAgentVersion
-		if errSearchLogs != nil || logAgentVersion == "" {
-			logElementNode.Logfile = logElement.FilePath + logElement.FileName
-			logElementNode.AgentVersion = ""
-			logElementNode.MatchFound = false
-		} else {
-			logElementNode.Logfile = logElement.FilePath + logElement.FileName
-			logElementNode.AgentVersion = logAgentVersion
-			logElementNode.MatchFound = true
-		}
-		agentVersion = append(agentVersion, logElementNode)
-		log.Debug("Agent version is", logAgentVersion)
-	}
-	return
+func getNodeVerFromPayload(payload []NodeModuleVersion) (string) {
+	// for _, logElement := range logs {
+	// 	logAgentVersion, errSearchLogs := searchLogs(logElement)
+	// 	var logElementNode logNodeAgentVersion
+	// 	if errSearchLogs != nil || logAgentVersion == "" {
+	// 		logElementNode.Logfile = logElement.FilePath + logElement.FileName
+	// 		logElementNode.AgentVersion = ""
+	// 		logElementNode.MatchFound = false
+	// 	} else {
+	// 		logElementNode.Logfile = logElement.FilePath + logElement.FileName
+	// 		logElementNode.AgentVersion = logAgentVersion
+	// 		logElementNode.MatchFound = true
+	// 	}
+	// 	agentVersion = append(agentVersion, logElementNode)
+	// 	log.Debug("Agent version is", logAgentVersion)
+	// }
+	// return
+	payload
+
+
 }
 
 func searchLogs(logElement logtask.LogElement) (string, error) {
 	searchString := `Node[.]js[.] Agent version:\s*([0-9.]+);`
 	filepath := logElement.FilePath + logElement.FileName
 	agentVersion, err := tasks.ReturnLastStringSubmatchInFile(searchString, filepath)
+
+
+
+	// cmdOutput, cmdError := p.cmdExec("npm", "ls", "--parseable=true", "--long=true", "--depth=0")
+	// modulesList := string(cmdOutput)
+	// if cmdError != nil {
+	// 	return modulesList, cmdError
+	// }
+	// return modulesList, nil
+
 	if err != nil {
 		return "", err
 	}
