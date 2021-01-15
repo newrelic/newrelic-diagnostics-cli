d99afb572efa2c93769e5e33be705005fd9bf9c1
uturunku1 <21225410+uturunku1@users.noreply.github.com>
2021-01-14 17:52:25 -0800

    finalize rollback script pointing to test directory

--- scripts/rollback.sh
+++ scripts/rollback.sh
@@ -3,16 +3,21 @@ set -e
 
 CURRENT=$(cat releaseVersion.txt | awk -F'currentReleaseVersion=' '{printf$2}')
 CURRENTZIP="nrdiag_${CURRENT}.zip"
-echo "my currentzip:"
-echo nrdiag_${CURRENT}.zip
-echo $CURRENTZIP
+
 PREV=$(cat releaseVersion.txt | awk -F'prevReleaseVersion=' '{printf$2}')
 PREVZIP="nrdiag_${PREV}.zip"
-echo "my prevzip:"
-echo nrdiag_${PREV}.zip
-aws s3 rm s3://${{ secrets.S3_BUCKET }}/nrdiag/test/${CURRENTZIP}
-echo "current release was removed"
-aws s3 cp s3://${{ secrets.S3_BUCKET }}/nrdiag/test/${PREVZIP} s3://${{ secrets.S3_BUCKET }}/nrdiag/test/nrdiag_latest.zip
-echo "previous release is now latest zip"
+
+echo "deleting current release zip file ..."
+aws s3 rm s3://${S3_BUCKET}/nrdiag/test/${CURRENTZIP}
+
+echo "replacing nrdiag_latest.zip to be the previous one ..."
+#--copy-props metadata-directive parameter is to avoid calling GetObjectTagging permission
+aws s3 cp s3://${S3_BUCKET}/nrdiag/test/${PREVZIP} s3://${S3_BUCKET}/nrdiag/test/nrdiag_latest.zip --copy-props metadata-directive
+
+echo "deleting version.txt ..."
+aws s3 rm s3://${S3_BUCKET}/nrdiag/test/version.txt
+echo "creating a new version.txt inserting the previous version ..."
 echo ${PREV} >> version.txt
-aws s3 cp version.txt s3://${{ secrets.S3_BUCKET }}/nrdiag/test/version.txt
+aws s3 cp version.txt s3://${S3_BUCKET}/nrdiag/test/version.txt
+echo "version is now:"
+aws s3 cp s3://nr-downloads-main/nrdiag/test/version.txt -
