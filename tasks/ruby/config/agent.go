7eeb11aa4caa8f63a13d3590aebdf30c0a4b3dec
uturunku1 <21225410+uturunku1@users.noreply.github.com>
2021-02-05 15:07:13 -0800

    add one more missing slice from a payload

--- tasks/ruby/config/agent.go
+++ tasks/ruby/config/agent.go
@@ -41,17 +41,17 @@ func (p RubyConfigAgent) Dependencies() []string {
 func (p RubyConfigAgent) Execute(options tasks.Options, upstream map[string]tasks.Result) tasks.Result { //By default this task is commented out. To see it run go to the tasks/registerTasks.go file and uncomment the w.Register for this task
 	var result tasks.Result //This is what we will use to pass the output from this task back to the core and report to the UI
 
-	if upstream["Base/Config/Validate"].HasPayload(){
+	if upstream["Base/Config/Validate"].HasPayload() {
 		validations, ok := upstream["Base/Config/Validate"].Payload.([]config.ValidateElement) //This is a type assertion to cast my upstream results back into data I know the structure of and can now work with. In this case, I'm casting it back to the []validateElements{} I know it should return
 		if !ok {
 			return tasks.Result{
-				Status: tasks.Error,
+				Status:  tasks.Error,
 				Summary: tasks.AssertionErrorSummary,
 			}
 		}
-	
+
 		rubyValidation, checkValidationTrue := checkValidation(validations)
-	
+
 		if checkValidationTrue {
 			log.Debug("Identified Ruby from validated config file, setting Ruby to true")
 			result.Status = tasks.Success
@@ -62,11 +62,11 @@ func (p RubyConfigAgent) Execute(options tasks.Options, upstream map[string]task
 	}
 
 	//If this fails to identify the language, now check the raw file itself
-	if upstream["Base/Config/Collect"].Status == tasks.Success{
+	if upstream["Base/Config/Collect"].Status == tasks.Success {
 		configs, ok := upstream["Base/Config/Collect"].Payload.([]config.ConfigElement) //This is a type assertion to cast my upstream results back into data I know the structure of and can now work with. In this case, I'm casting it back to the []validateElements{} I know it should return
 		if !ok {
 			return tasks.Result{
-				Status: tasks.Error,
+				Status:  tasks.Error,
 				Summary: tasks.AssertionErrorSummary,
 			}
 		}
@@ -84,6 +84,7 @@ func (p RubyConfigAgent) Execute(options tasks.Options, upstream map[string]task
 				rubyItem := config.ValidateElement{Config: configItem, Status: tasks.None} //This defines the mocked validate element we'll put in the results that is empty expect the config element
 				validationResults = append(validationResults, rubyItem)
 			}
+			result.Payload = validationResults
 			return result
 		}
 	}
