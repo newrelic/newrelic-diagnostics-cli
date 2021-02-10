4f7a80dda1d9b15ea6ff5ef8e744a6c268299eb5
uturunku1 <21225410+uturunku1@users.noreply.github.com>
2021-02-10 12:17:47 -0800

    remove check for upstream[Base/Config/Validate].HasPayload() from task because this validation was already accounted for in its upstream task

--- tasks/dotnet/config/agent_windows.go
+++ tasks/dotnet/config/agent_windows.go
@@ -59,14 +59,7 @@ func (p DotNetConfigAgent) Execute(options tasks.Options, upstream map[string]ta
 		}
 	}
 
-	if !upstream["Base/Config/Validate"].HasPayload() {
-		return tasks.Result{
-			Status:  tasks.None,
-			Summary: ".NET Agent config file was not found and validated. This task did not run",
-		}
-	}
-
-	// get all the config files and elements to check them
+	// get all the config files and elements to check them. No need to verify if this task succeeded because we already checked this on the upstream task DotNet/Agent/Installed
 	configFiles, ok := upstream["Base/Config/Validate"].Payload.([]config.ValidateElement) //This is a type assertion to cast my upstream results back into data I know the structure of and can now work with. In this case, I'm casting it back to the []validateElements{} I know it should return
 	if !ok {
 		return tasks.Result{
