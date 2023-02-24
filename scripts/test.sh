1b8ebaafcf3544780594d14f61e0f163cf4b73a5
David Affinito <daffinito@newrelic.com>
2023-02-24 12:37:29 -0800

    feat: Infra/Agent/Connect - updated with new endpoints

--- scripts/test.sh
+++ scripts/test.sh
@@ -13,6 +13,7 @@ fi
 go get -t ./...
 go install -mod=mod github.com/onsi/ginkgo/v2/ginkgo@latest
 go get github.com/onsi/ginkgo/v2
+go mod tidy
 go build
 
 ginkgo --skip-package dotnet/agent,dotnet/requirements,dotnet/env --no-color --keep-going -r -timeout=1h
