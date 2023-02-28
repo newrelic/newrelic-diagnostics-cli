a2b136236740e4e096d58b563f35db10668c1898
David Affinito <daffinito@newrelic.com>
2023-02-28 09:54:54 -0800

    fix: Rebase and fix unit test

--- scripts/test.sh
+++ scripts/test.sh
@@ -16,7 +16,7 @@ go get github.com/onsi/ginkgo/v2
 go mod tidy
 go build
 
-ginkgo --skip-package dotnet/agent,dotnet/requirements,dotnet/env --no-color --keep-going -r -timeout=1h
+ginkgo --skip-package dotnet/agent,dotnet/requirements,dotnet/env,dotnet/profiler --no-color --keep-going -r -timeout=1h
 
 # This can be run from the madhatter-build dockerfile with
 # docker run --rm madhatter-build:latest ./publish.sh
